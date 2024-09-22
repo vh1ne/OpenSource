bc43ae81677bf9893610aab0f769b4b51b8dfbc8
vh1ne <venkatesh.honrao@gmail.com>
2024-09-22 23:15:22 +0530

    initial commit

---
+++ src/main/java/com/vh1ne/OpenSource/OpenSourceApplication.java
@@ -0,0 +1,13 @@
+package com.vh1ne.OpenSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSourceApplication.class, args);
+	}
+
+}
