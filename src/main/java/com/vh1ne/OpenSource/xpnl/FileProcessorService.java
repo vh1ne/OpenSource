80f43da829e423daf814318db45924bb07dc2046
vh1ne <venkatesh.honrao@gmail.com>
2025-02-08 20:46:09 +0530

    synchronization block replaced with lock

--- src/main/java/com/vh1ne/OpenSource/xpnl/FileProcessorService.java
+++ src/main/java/com/vh1ne/OpenSource/xpnl/FileProcessorService.java
@@ -20,12 +20,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @Log4j2
 public class FileProcessorService {
 
-
+    Random random = new Random();
     public String generate300k() {
         String outputFileName = "dummy_requests.txt"; // Output file
         String outputFilePath = Paths.get(System.getProperty("user.dir"), outputFileName).toString(); // Full path to the file
@@ -34,20 +36,14 @@ public class FileProcessorService {
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
             ObjectMapper objectMapper = new ObjectMapper();
-            Random random = new Random();
-
-            for (int i = 0; i < totalRequests; i++) {
+               for (int i = 0; i < totalRequests; i++) {
                 Map<String, Object> jsonRequest = new HashMap<>();
                 jsonRequest.put("id", i + 1);
                 jsonRequest.put("name", "User" + (i + 1));
                 jsonRequest.put("email", "user" + (i + 1) + "@example.com");
                 jsonRequest.put("age", random.nextInt(60) + 18); // Age between 18 and 77
                 jsonRequest.put("country", getRandomCountry(random));
-
-                // Convert the map to a JSON string
                 String jsonString = objectMapper.writeValueAsString(jsonRequest);
-
-                // Write JSON string to the file
                 writer.write(jsonString);
                 writer.newLine();
 
@@ -98,12 +94,16 @@ public class FileProcessorService {
                 })
                 .subscribe();
     }
-
-    private Mono<Integer> writeToFile(BufferedWriter writer, String response) {
+    private final Lock lock = new ReentrantLock();
+    public Mono<Integer> writeToFile(BufferedWriter writer, String response) {
         return Mono.fromCallable(() -> {
-            synchronized (writer) { // Synchronize writes
+            lock.lock();
+            try {
                 writer.write(response);
                 writer.newLine();
+                writer.flush(); // Ensure data is written immediately
+            } finally {
+                lock.unlock();
             }
             return 1;
         }).subscribeOn(Schedulers.boundedElastic()); // Offload I/O operations
